5d738b4e9f84159a2290f67b7c2712dd0e57acb0
Rrr <rojitrajbanshi69@gmail.com>
2026-06-24 10:18:44 +0545

    Add drum sound buttons with audio playback

---
+++ drum.js
@@ -0,0 +1,21 @@
+document.querySelectorAll("button")[0].addEventListener("click",function (){
+        var audio= new Audio("sounds/tom-1.mp3");
+    audio.play()});
+document.querySelectorAll("button")[1].addEventListener("click",function (){
+        var audio1=new Audio("sounds/tom-2.mp3");
+    audio1.play()});
+document.querySelectorAll("button")[2].addEventListener("click",function (){
+       var audio2=new Audio("sounds/tom-3.mp3");
+    audio2.play()});
+document.querySelectorAll("button")[3].addEventListener("click",function (){
+       var audio3=new Audio("sounds/tom-4.mp3");
+    audio3.play()});
+document.querySelectorAll("button")[4].addEventListener("click",function (){
+        var audio4=new Audio("sounds/crash.mp3");
+    audio4.play()});
+document.querySelectorAll("button")[5].addEventListener("click",function (){
+       var audio5=new Audio("sounds/kick-bass.mp3");
+    audio5.play()});
+document.querySelectorAll("button")[6].addEventListener("click",function (){
+       var audio6=new Audio("sounds/snare.mp3");
+    audio6.play()});
