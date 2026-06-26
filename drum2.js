03717b398ad16a51022f056235bc5247e502da52
Rojit tech 69 <rojitrajbanshi69@gmail.com>
2026-06-26 12:32:35 +0545

    Correct buttonAnimation function syntax and added timeout for animation
    Fix buttonAnimation function syntax and add missing parentheses.

--- drum2.js
+++ drum2.js
@@ -52,5 +52,7 @@ function makeSound(key) {
 function buttonAnimation(currentkey)
 var activeButton= document.querySelector("."+currentKey);
 activeButton.classList.add("pressed");
+setTimeout(function){
+    activeButton.classList.remove("pressed");},100);    
 
 }
